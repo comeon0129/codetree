758bb644c3304ff9adc9f7b991613bedfaeef2b6
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2024-10-13 17:19:13 +0900

    Title: 소수점 반올림하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241013/소수점 반올림하기/rounding-decimal-points.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 변수 선언
+    double a = 25.352;
+	
+	cout << fixed;
+	cout.precision(1);
+    
+    // 출력
+    cout << a;
+    return 0;
+}
