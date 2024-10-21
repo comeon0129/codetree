d0e38c722858153587a69394d1433b30e2a409be
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2024-10-21 21:30:32 +0900

    Title: 전화번호 바꾸기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241021/전화번호 바꾸기/change-phone-number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int first, second, third;
+    cin>>first;
+    cin.get();
+    cin>>second;
+    cin.get();
+    cin>>third;
+    cout<<"0"<<first<<"-"<<third<<"-"<<second;
+    return 0;
+}
