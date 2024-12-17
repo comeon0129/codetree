93b59072e73a8605c5e01c9e715b90a3245466f5
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2024-12-18 00:49:21 +0900

    Title: 굉장한 수, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 241218/굉장한 수/amazing-number.java
+++ 241218/굉장한 수/amazing-number.java
@@ -6,7 +6,7 @@ public class Main {
 	public static void main(String[]args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		if ((a % 2 == 0 && a % 3 ==0) || (a % 2 == 0 && a % 5 == 0)){
+		if ((a % 2 != 0 && a % 3 ==0) || (a % 2 == 0 && a % 5 == 0)){
 			System.out.println("true");
 		}
 		else
