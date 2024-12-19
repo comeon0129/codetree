22711074e3ce363396572a02b6e8cdd195b1b049
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2024-12-19 18:44:26 +0900

    Title: 증가시키며 출력하기 2, Time: 82ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241219/증가시키며 출력하기 2/increase-and-print-2.java
@@ -0,0 +1,16 @@
+public class Main {
+	
+	
+	public static void main(String[]args) {
+//		Scanner sc = new Scanner(System.in);
+//		int b = sc.nextInt();
+//		int a = sc.nextInt();
+		int i=10;
+		while(i<27) {
+			System.out.print(i+++" ");
+		
+		}
+		
+		
+	}
+}
