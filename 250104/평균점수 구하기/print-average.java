e9d8fc123097e539bd1cf3a2af7a701c6d4bc749
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-01-04 22:04:22 +0900

    Title: 평균점수 구하기, Time: 145ms, Memory: 12MB, Status: Passed - CodeTree

---
+++ 250104/평균점수 구하기/print-average.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+
+public class Main {
+	
+	
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		double sum = 0;
+		for(int i=0; i<8; i++) {
+			sum+= sc.nextDouble();
+		}
+		double mid = sum/8;
+		System.out.printf("%.1f",mid);
+
+		
+		
+		
+		sc.close();
+	}
+		
+}
