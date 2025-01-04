d68f73f6e7ddad5a088ebabae4294656ec7caa55
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-01-04 17:54:18 +0900

    Title: 알파벳 출력하기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250104/알파벳 출력하기/print-alphabet.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+
+public class Main {
+	
+	
+	public static void main(String[]args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();	
+		int cnt = 65;
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=1; j<=i; j++) {
+				System.out.print((char)cnt);
+				cnt++;
+				if(cnt=='Z'+1)
+					cnt='A';
+			}
+			System.out.println();
+		}
+			
+		sc.close();
+	}
+		
+}
