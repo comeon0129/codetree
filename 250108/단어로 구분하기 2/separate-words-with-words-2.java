59a20fd17b27b8c1b3a42ecb84f49c9a2365ae96
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-01-08 12:06:57 +0900

    Title: 단어로 구분하기 2, Time: 107ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250108/단어로 구분하기 2/separate-words-with-words-2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	
+    	for(int i=0; i<10; i++) {
+    		String s = sc.next();
+    		if(i%2==0) {
+    			System.out.println(s);
+    		}
+    		
+    		
+    	}
+    	
+    	
+        sc.close();
+    }
+}
