3ae82cf96625d345a5e77c56468fab893d95f22d
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-01-08 19:27:17 +0900

    Title: 첫 번째와 두 번째 교환, Time: 123ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250108/첫 번째와 두 번째 교환/exchange-1st-and-2nd.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	String s = sc.next();
+    	char[] arr = s.toCharArray();
+    	char a = arr[0];
+    	char b = arr[1];
+    	for(int i=0; i<arr.length; i++) {
+    		if(arr[i] == a) {
+    			arr[i] = b;
+    		}
+    		else if(arr[i] == b) {
+    			arr[i] = a;
+    		}
+    	}
+    	s=String.valueOf(arr);
+    	System.out.println(s);
+    	
+        sc.close();
+    }
+}
