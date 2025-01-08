e665157fc198a577c692a63b4596b5d51287fe7f
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-01-08 19:41:26 +0900

    Title: e 제거하기, Time: 118ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 250108/e 제거하기/e-to-remove.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	String s = sc.next();
+    	int idx = s.indexOf('e');
+    	s= s.substring(0,idx)+s.substring(idx+1);
+    	System.out.println(s);
+    	
+    	
+    	
+        sc.close();
+    }
+}
