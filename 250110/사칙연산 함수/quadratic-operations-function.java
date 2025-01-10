59673ae45a40478c8ef2719d51033c577c5f8f22
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-01-10 20:36:25 +0900

    Title: 사칙연산 함수, Time: 111ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250110/사칙연산 함수/quadratic-operations-function.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void plus(int a, int b) {
+		System.out.println(a+" + "+b+" = "+a+b);
+	}
+	public static void minus(int a, int b) {
+		System.out.println(a+" - "+b+" = "+(a-b));
+	}
+	public static void mult(int a, int b) {
+		System.out.println(a+" * "+b+" = "+a*b);
+	}
+	public static void divide(int a, int b) {
+		System.out.println(a+" / "+b+" = "+a/b);
+	}
+	
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int a = sc.nextInt();
+    	char c= sc.next().charAt(0);
+    	int b = sc.nextInt();
+    	if(c == '+') {
+    		plus(a,b);
+    	}
+    	else if(c == '-') {
+    		minus(a,b);
+    	}
+    	else if(c == '*') {
+    		mult(a,b);
+    	}
+    	else if(c == '/') {
+    		divide(a,b);
+    	}
+    	else {
+    		System.out.println("False");
+    	}
+        sc.close();
+    }
+}
