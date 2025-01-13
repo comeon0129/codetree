9d2c8186bbbd18801fda2287bb46df9310c004aa
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-01-14 00:18:31 +0900

    Title: 문자열 정렬, Time: 100ms, Memory: 9MB, Status: Runtime Error - CodeTree

---
+++ 250114/문자열 정렬/string-sort.java
@@ -0,0 +1,20 @@
+package java_study;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s =sc.next();
+		char[] arr = s.toCharArray();
+		Arrays.sort(arr);
+		s = String.valueOf(arr);
+		System.out.println(s);
+		
+		
+		
+		sc.close();
+    }
+}
