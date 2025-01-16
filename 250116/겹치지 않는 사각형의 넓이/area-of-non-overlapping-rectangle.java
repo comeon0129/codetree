b4a0e5b0b131ce9bb6241a1967e51aaf0b974fcf
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-01-16 16:01:31 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 140ms, Memory: 26MB, Status: Runtime Error - CodeTree

---
+++ 250116/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static final int MAX_R = 2000;
+	public static final int OFFSET = 1000;
+	
+	
+	public static int[][] checked = new int[MAX_R+1][MAX_R+1];
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a_x1= sc.nextInt();
+		int a_y1= sc.nextInt();
+		int a_x2= sc.nextInt();
+		int a_y2= sc.nextInt();
+		
+		for(int x=a_x1; x<a_x2; x++) {
+			for(int y =a_y1; y<a_y2; y++) {
+				checked[x][y] = 1;
+			}
+		}
+		
+		int b_x1= sc.nextInt();
+		int b_y1= sc.nextInt();
+		int b_x2= sc.nextInt();
+		int b_y2= sc.nextInt();
+		
+		for(int x=b_x1; x<b_x2; x++) {
+			for(int y =b_y1; y<b_y2; y++) {
+				checked[x][y] = 1;
+			}
+		}
+		
+		int m_x1= sc.nextInt();
+		int m_y1= sc.nextInt();
+		int m_x2= sc.nextInt();
+		int m_y2= sc.nextInt();
+		
+		for(int x=m_x1; x<m_x2; x++) {
+			for(int y =m_y1; y<m_y2; y++) {
+				checked[x][y] = 0;
+			}
+		}
+
+		
+		int area =0;
+		for(int i=0; i<=MAX_R; i++) {
+			for(int j=0; j<=MAX_R; j++) {
+				if(checked[i][j] == 1) {
+					area++;
+				}
+			}
+		}
+		System.out.println(area);
+				
+		sc.close();
+    }
+}
