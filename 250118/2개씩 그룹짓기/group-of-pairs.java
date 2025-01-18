4635516071c231ba6b144ca95198482dc709170c
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-01-18 16:34:22 +0900

    Title: 2개씩 그룹짓기, Time: 202ms, Memory: 13MB, Status: Passed - CodeTree

---
+++ 250118/2개씩 그룹짓기/group-of-pairs.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+
+public class Main {
+
+	public static final int INT_MIN = Integer.MIN_VALUE;
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[2*n];
+	
+		for(int i=0; i<2*n; i++) {
+			arr[i] = sc.nextInt();
+		}
+		Arrays.sort(arr);
+		int max = INT_MIN;
+		
+		for(int i=0; i<2*n; i++) {
+			if(arr[i]+ arr[2*n-i-1] > max) {
+				max = arr[i] + arr[2*n-i-1];
+			}
+		}
+		
+		
+		System.out.println(max);
+		
+		sc.close();
+    }
+}
