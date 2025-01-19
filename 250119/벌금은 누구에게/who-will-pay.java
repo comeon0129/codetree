41ddd32d0dc0e4065613cd2c0d8cac1761a0a51e
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-01-19 21:12:58 +0900

    Title: 벌금은 누구에게, Time: 193ms, Memory: 12MB, Status: Passed - CodeTree

--- 250119/벌금은 누구에게/who-will-pay.java
+++ 250119/벌금은 누구에게/who-will-pay.java
@@ -9,15 +9,19 @@ public class Main {
 		int m = sc.nextInt();
 		int k = sc.nextInt();
 		int[] count = new int[n+1];
+		int[] input = new int[m];
 		int ans = -1;
 		for(int i=1; i<=n; i++) {
 			count[i] = k;
 		}
 		for(int i=0; i<m; i++) {
-			int num = sc.nextInt();
-			count[num]--;
-			if(count[num] == 0) {
-				ans = num;
+			input[i] = sc.nextInt();
+		}
+		for(int i=0; i<m; i++) {
+			count[input[i]]--;
+			if(count[input[i]] == 0) {
+				ans = input[i];
+				break;
 			}
 		}
 		System.out.println(ans);
