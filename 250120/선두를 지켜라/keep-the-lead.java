90321c08c1fd89758af1a9c3bd333192a72e9c3f
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-01-20 14:55:50 +0900

    Title: 선두를 지켜라, Time: 234ms, Memory: 28MB, Status: Passed - CodeTree

--- 250120/선두를 지켜라/keep-the-lead.java
+++ 250120/선두를 지켜라/keep-the-lead.java
@@ -44,7 +44,7 @@ public class Main {
         }
         int ans = 0;
         for(int i=1; i<totalTimeA; i++) {
-        	if(arr[i] != arr[i+1]) {
+        	if(arr[i] != 0 && arr[i] != arr[i+1]) {
         		ans++;
         	}
         }
