360537d8a83a4cd6f228473e3af9bb5250f5d0b7
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-01-25 16:37:46 +0900

    Title: 아름다운 수열 2, Time: 142ms, Memory: 10MB, Status: Passed - CodeTree

--- 250125/아름다운 수열 2/beautiful-sequence-2.java
+++ 250125/아름다운 수열 2/beautiful-sequence-2.java
@@ -5,6 +5,7 @@ public class Main {
 	public static int n,m;
 	public static int[] arr = new int[100];
 	public static int[] beauty = new int[100];
+	public static int[] used = new int[100];
 	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
@@ -16,13 +17,15 @@ public class Main {
 		}
 		for(int i=0; i<m; i++) {
 			beauty[i] = sc.nextInt();
+			used[i] = 1;
 		}
 		
 		for(int i=0; i<=n-m; i++) {
 			int cnt =0;
 			for(int j=i; j<i+m; j++) {
 				for(int k=0; k<m; k++) {
-					if(beauty[k] == arr[j]) {
+					if(beauty[k] == arr[j] && used[k] == 1) {
+						used[k] = 0;
 						cnt++;
 						break;
 					}
@@ -30,6 +33,9 @@ public class Main {
 			}
 			if(cnt == m) {
 				ans++;
+			}
+			for(int j=0; j<m; j++) {
+				used[j] = 1;
 			}
 		}
 		System.out.println(ans);
