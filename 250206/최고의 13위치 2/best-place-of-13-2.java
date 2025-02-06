835f780159c75d3ee1f3e28d21824e1a9aac88de
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-02-06 19:28:01 +0900

    Title: 최고의 13위치 2, Time: 115ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250206/최고의 13위치 2/best-place-of-13-2.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+	public static final int MAX_N = 20;
+	public static int[][] board = new int[MAX_N][MAX_N];
+	public static int n;
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				board[i][j] = sc.nextInt();
+			}
+		}
+		int ans = 0;
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n-2; j++) {
+				for(int k=0; k<n; k++) {
+					for(int l=0; l<n-2; l++) {
+						if(i == k && l <= i+2) { // 두 격자가 같은 행에 있는데 겹치는 경우
+							continue;
+						}
+						ans = Math.max(ans, board[i][j]+board[i][j+1]+board[i][j+2]+board[k][l]+board[k][l+1]+board[k][l+2]);
+					}
+				}
+			}
+		}
+		System.out.println(ans);
+		
+		
+		sc.close();
+    }
+}
