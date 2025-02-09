65c9c4102536b1a50bfd5276225b18d016b7fd02
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-02-09 20:00:40 +0900

    Title: 스승의 은혜 2, Time: 120ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250209/스승의 은혜 2/the-grace-form-teacher-2.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+	public static int MAX_N = 1000;
+	
+	public static int n,b;  // n: 학생수 , b: 예산
+	
+	public static int[] p = new int[MAX_N];
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		b = sc.nextInt();
+		
+		for(int i=0; i<n; i++) {
+			p[i] = sc.nextInt();
+		}
+		
+		Arrays.sort(p,0,n);
+		
+		int maxCnt = 0;
+		for(int i=0; i<n; i++) {
+			int cnt = 0;
+			int curB = b;
+			p[i] /=2;
+			for(int j=0; j<n; j++) {
+				curB -= p[j];
+				if(curB <= 0) {
+					break;
+				}
+				cnt++;
+			}
+			maxCnt = Math.max(maxCnt, cnt);
+			p[i] *=2;
+		}
+		
+		System.out.println(maxCnt);
+		
+		
+		sc.close();
+    }
+}
