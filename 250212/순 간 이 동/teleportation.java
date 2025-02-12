f7b99e334c8169e2f48fed82591edf291be12d25
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-02-12 11:53:58 +0900

    Title: 순 간 이 동, Time: 122ms, Memory: 10MB, Status: Passed - CodeTree

--- 250212/순 간 이 동/teleportation.java
+++ 250212/순 간 이 동/teleportation.java
@@ -12,7 +12,9 @@ public class Main {
 		
 		int dist1 = Math.abs(y-a)+Math.abs(b-x);
 		int dist2 = Math.abs(x-a)+Math.abs(b-y);
-		int ans = Math.min(dist1, dist2);
+		int dist3 = Math.abs(a-b);
+		int temp = Math.min(dist1, dist2);
+		int ans = Math.min(temp, dist3);
 		System.out.println(ans);
 		
 		sc.close();
