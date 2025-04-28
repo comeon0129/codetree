d9a0a96404a409ac7775de540ce4db2795c6f7cf
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-04-28 11:03:14 +0900

    Title: 블럭쌓는 명령2, Time: 142ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250428/블럭쌓는 명령2/block-stacking-commands2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        for (int i = 0; i < K; i++) {
+            int A = sc.nextInt();
+            int B = sc.nextInt();
+        }
+        // Please write your code here.
+    }
+}
