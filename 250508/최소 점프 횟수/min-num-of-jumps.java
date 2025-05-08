364d0c2a122063b4f4179d0260b46f2e7b05e1af
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-05-08 18:55:51 +0900

    Title: 최소 점프 횟수, Time: 138ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250508/최소 점프 횟수/min-num-of-jumps.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main {
+	public static int n;
+	
+	public static int[] arr = new int[10];
+	
+	public static int ans = Integer.MAX_VALUE;
+	
+	public static boolean isPossible = false;
+	
+	public static void findMin(int index, int cnt) {
+		if(index == n-1) {
+			isPossible = true;
+			ans = Math.min(ans, cnt);
+			return;
+		}
+		
+		for(int i=1; i<=arr[index]; i++) {
+			findMin(index+i,cnt+1);
+		}
+		
+	}
+	
+	
+	public static void main (String args[]) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        
+        for(int i=0; i<n; i++) {
+        	arr[i] = sc.nextInt();
+        }
+        
+        findMin(0,0);
+        if(!isPossible) {
+        	System.out.println(-1);
+        }
+        else {
+        	System.out.println(ans);
+        }
+        
+        sc.close();
+    }
+}
