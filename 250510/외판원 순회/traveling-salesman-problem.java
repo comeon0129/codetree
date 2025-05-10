19928892fdef8c9ccdae1977400218feab8fbf31
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-05-10 10:03:17 +0900

    Title: 외판원 순회, Time: 133ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250510/외판원 순회/traveling-salesman-problem.java
@@ -0,0 +1,69 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static int n;
+	
+	public static boolean[] visited = new boolean[10];
+	
+	public static int[][] cost = new int [10][10];
+	
+	public static ArrayList<Integer> answer = new ArrayList<>();
+	
+	public static int ans = Integer.MAX_VALUE;
+	
+	public static void calcMinSum() {
+		int sum = 0;
+		
+		for(int i=0; i<n; i++) {
+			if(cost[i][answer.get(i)]== 0)
+				return;
+			
+			sum += cost[i][answer.get(i)];
+		}
+		
+		ans = Math.min(ans, sum);
+		
+		return;
+	}
+	
+	public static void findMin(int curNum) {
+		if(curNum == n) {
+			calcMinSum();
+			return;
+		}
+		
+		for(int i=0; i<n; i++) {
+			if(visited[i])
+				continue;
+			
+			visited[i] = true;
+			answer.add(i);
+			
+			findMin(curNum+1);
+			
+			answer.remove(answer.size()-1);
+			visited[i] = false;
+			
+		}
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				cost[i][j] =sc.nextInt();
+			}
+		}
+		
+		findMin(0);
+		
+		System.out.println(ans);
+		
+		sc.close();
+	}
+}
