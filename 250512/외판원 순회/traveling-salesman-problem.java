18093c299a9735ddcece4d38c01598205ed77307
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-05-12 15:26:49 +0900

    Title: 외판원 순회, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250512/외판원 순회/traveling-salesman-problem.java
+++ 250512/외판원 순회/traveling-salesman-problem.java
@@ -1,78 +1,73 @@
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
-	
-	public static int n;
-	
-	public static boolean[] visited = new boolean[10];
-	
-	public static int[][] cost = new int [10][10];
-	
-	public static ArrayList<Integer> y_pos = new ArrayList<>();
-	public static ArrayList<Integer> x_pos = new ArrayList<>();
-	
-	public static int ans = Integer.MAX_VALUE;
-	
-	
-	public static void calcMinSum(int curX) {
-		int sum = 0;
-		
-		for(int i=0; i<n-1; i++) {
-			sum += cost[x_pos.get(i)][y_pos.get(i)];
-		}
-		
-		if(cost[curX][0] == 0)
-			return;
-		else {
-			sum+=cost[curX][0];
-		}
+    public static final int INT_MAX = Integer.MAX_VALUE;
+    public static final int MAX_N = 10;
+    
+    // 변수 선언
+    public static int n;
+    public static int[][] cost = new int[MAX_N][MAX_N];
+    public static boolean[] visited = new boolean[MAX_N];
+    public static ArrayList<Integer> picked = new ArrayList<>();
+    
+    public static int ans = INT_MAX;
+    
+    // 지금까지 방문한 지점의 수를 cnt라 했을 때
+    // 계속 탐색을 이어서 진행합니다.
+    public static void findMin(int cnt) {
+        // 모든 지점를 방문했을 때
+        // 가능한 지금까지의 비용 합 중
+        // 최솟값을 갱신합니다.
+        if(cnt == n) {
+            int totalCost = 0;
+            for(int i = 0; i < picked.size() - 1; i++) {
+                int currCost = cost[picked.get(i)][picked.get(i + 1)];
+                // 만약 비용이 0이라면 불가능한 경우이므로 
+                // 바로 함수를 빠져나옵니다.
+                if(currCost == 0)
+                    return;
+                
+                totalCost += currCost;
+            }
+    
+            // 최종적으로 1번 지점으로 다시 돌아오는 것 까지 고려해서 계산해줍니다.
+            int lastPos = picked.get(picked.size() - 1);
+            int additionalCost = cost[lastPos][0];
+            if(additionalCost == 0)
+                return;
+    
+            // 답을 갱신해줍니다.
+            ans = Math.min(ans, totalCost + additionalCost);
+            return;
+        }
+    
+        // 방문할 지점을 선택합니다.
+        for(int i = 0; i < n; i++) {
+            if(visited[i]) continue;
+            visited[i] = true;
+            picked.add(i);
+    
+            findMin(cnt + 1);
+    
+            visited[i] = false;
+            picked.remove(picked.size() - 1);
+        }
+    }
 
-		ans = Math.min(ans, sum);
-		
-		return;
-	}
-	
-	public static void findMin(int cnt, int curX) {
-		
-		if(cnt == n-1) {
-			calcMinSum(curX);
-			return;
-		}
-		
-	
-		for(int i=0; i<n; i++) {
-			if(visited[i] || cost[curX][i] == 0)
-				continue;
-				
-			visited[curX] = true;
-				
-			x_pos.add(curX);
-			y_pos.add(i);
-				
-			findMin(cnt+1,i);
-				
-			x_pos.remove(x_pos.size()-1);
-			y_pos.remove(y_pos.size()-1);
-			visited[curX] = false;
-				
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // 입력:
+        n = sc.nextInt();
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
-		for(int i=0; i<n; i++) {
-			for(int j=0; j<n; j++) {
-				cost[i][j] =sc.nextInt();
-			}
-		}
-		
-		
-		findMin(0,0);
-		
-		System.out.println(ans);
-		
-		sc.close();
-	}
-}
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                cost[i][j] = sc.nextInt();
+
+        // 1번 지점을 시작으로
+        // 탐색을 진행합니다.
+        visited[0] = true;
+        picked.add(0);
+        findMin(1);
+
+        System.out.print(ans);
