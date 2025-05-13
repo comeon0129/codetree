5bed02ee3304cbfe7d8d6b0dbd89eb74faddf623
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-05-13 16:29:45 +0900

    Title: 피보나치 수, Time: 131ms, Memory: 10MB, Status: Passed - Codetree

--- 250513/피보나치 수/fibonacci-number.java
+++ 250513/피보나치 수/fibonacci-number.java
@@ -12,17 +12,19 @@ public class Main {
 		
 		dp = new int[n+1];
 		
-		dp[0] = 0;
-		dp[1] = 1;
-		dp[2] = 1;
-		
 		if(n>=3) {
+			dp[0] = 0;
+			dp[1] = 1;
+			dp[2] = 1;
+			
 			for(int i=3; i<=n; i++) {
 				dp[i] = dp[i-1]+dp[i-2];
 			}
+			System.out.println(dp[n]);
 		}
+		else
+			System.out.println(1);
 		
-		System.out.println(dp[n]);
 		
 		sc.close();
 	}
