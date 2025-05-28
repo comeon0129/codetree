b3d21c0b65ba47ab20503a65db0f48d8fde846be
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-05-28 15:35:57 +0900

    Title: 금 채굴하기, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

--- 250528/금 채굴하기/gold-mining.java
+++ 250528/금 채굴하기/gold-mining.java
@@ -4,55 +4,57 @@ public class Main {
 	public static int n; //격자 크기 
 	public static int m; //금 한개의 가격
 	public static int[][] grid;
+	public static int[][] used;
 	
+	public static int tempCnt = 0;
 	public static int maxCnt = 0; // 최대 채굴가능한 금의 개수
 	
 	public static boolean inRange(int x, int y) {
 		return 0<=x && x < n && 0<= y && y<n;
 	}
 	
-	public static int calcGold(int x, int y, int k) {
-		if(k == 0) {
-			if(grid[x][y] == 1)
-				return 1;
-			else
-				return 0;
-		}
+	public static boolean canGo(int x, int y) {
+		if(!inRange(x, y))
+			return false;
+		if(used[x][y] == 1)
+			return false;
+		return true;
+	}
+	
+	public static void calcGold(int x, int y, int k, int cnt) {
+		if(cnt > k)
+			return ;
 		
-		int gold = 0;
+		int[] dx = {-1,1,0,0}; //상하좌우 순서
+		int[] dy = {0,0,-1,1};
 		
-		//방사형으로 퍼지는 범위 부터 계산해주고
-		for(int i = x-k+1; i<x+k; i++) {
-			for(int j= y-k+1; j<y+k; j++) {
-				if(inRange(i, j) && grid[i][j] == 1)
-					gold++;
+		used[x][y] = 1;
+		
+		if(grid[x][y] == 1)
+			tempCnt++;
+		
+		for(int i=0; i<4; i++) {
+			int newX = x+dx[i];
+			int newY = y+dy[i];
+			
+			if(canGo(newX, newY)) {
+				calcGold(newX,newY,k,cnt+1);
 			}
 		}
-		// 끝에 꼬다리 네 부분 계산해주기
-		if(inRange(x-k, y) && grid[x-k][y] == 1) {
-			gold++;
-		}
-		if(inRange(x+k, y) && grid[x+k][y] == 1) {
-			gold++;
-		}
-		if(inRange(x, y-k) && grid[x][y-k] == 1) {
-			gold++;
-		}
-		if(inRange(x, y+k) && grid[x][y+k] == 1) {
-			gold++;
-		}
 		
-		return gold;
+		return ;
 	}
 	
 	
 	public static void calc(int k) {
 		for(int i=0; i<n; i++) {
 			for(int j=0; j<n; j++) {
-				int goldCnt = calcGold(i,j,k);
+				tempCnt = 0;					// 임시로 골드 개수 셀 변수 초기화
+				calcGold(i,j,k,0);			    // 일시적인 골드 개수 세기
+				used = new int[n][n];		// used 초기화
 				
-				if(goldCnt * m >=  (k*k)+(k+1)*(k+1)) {
-					maxCnt= Math.max(maxCnt, goldCnt);
+				if(tempCnt * m >=  (k*k)+(k+1)*(k+1)) {
+					maxCnt= Math.max(maxCnt, tempCnt);
 				}
 			}
 		}
@@ -62,6 +64,90 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 		grid = new int[n][n];
+		used = new int[n][n];
+		m = sc.nextInt();
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				grid[i][j] = sc.nextInt();
+			}
+		}
+		
+		for(int k=n; k>=0; k--) {
+			calc(k);
+		}
+		
+		System.out.println(maxCnt);
+		sc.close();
+	}
+}
+import java.util.Scanner;
+
+public class Main {
+	public static int n; //격자 크기 
+	public static int m; //금 한개의 가격
+	public static int[][] grid;
+	public static int[][] used;
+	
+	public static int tempCnt = 0;
+	public static int maxCnt = 0; // 최대 채굴가능한 금의 개수
+	
+	public static boolean inRange(int x, int y) {
+		return 0<=x && x < n && 0<= y && y<n;
+	}
+	
+	public static boolean canGo(int x, int y) {
+		if(!inRange(x, y))
+			return false;
+		if(used[x][y] == 1)
+			return false;
+		return true;
+	}
+	
+	public static void calcGold(int x, int y, int k, int cnt) {
+		if(cnt > k)
+			return ;
+		
+		int[] dx = {-1,1,0,0}; //상하좌우 순서
+		int[] dy = {0,0,-1,1};
+		
+		used[x][y] = 1;
+		
+		if(grid[x][y] == 1)
+			tempCnt++;
+		
+		for(int i=0; i<4; i++) {
+			int newX = x+dx[i];
+			int newY = y+dy[i];
+			
+			if(canGo(newX, newY)) {
+				calcGold(newX,newY,k,cnt+1);
+			}
+		}
+		
+		return ;
+	}
+	
+	
+	public static void calc(int k) {
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				tempCnt = 0;					// 임시로 골드 개수 셀 변수 초기화
+				calcGold(i,j,k,0);			    // 일시적인 골드 개수 세기
+				used = new int[n][n];		// used 초기화
+				
+				if(tempCnt * m >=  (k*k)+(k+1)*(k+1)) {
+					maxCnt= Math.max(maxCnt, tempCnt);
+				}
+			}
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		grid = new int[n][n];
+		used = new int[n][n];
 		m = sc.nextInt();
 		
 		for(int i=0; i<n; i++) {
