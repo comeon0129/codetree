f0374dc2b45755a98196abefacf8fe9ca913bf5c
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-05-31 00:06:22 +0900

    Title: 최단 Run Length 인코딩, Time: 116ms, Memory: 10MB, Status: Passed - Codetree

--- 250531/최단 Run Length 인코딩/shortest-run-length-encoding.java
+++ 250531/최단 Run Length 인코딩/shortest-run-length-encoding.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 
@@ -25,7 +26,7 @@ public class Main {
 					length++;
 				}
 			}
-			if(length == 1)//모든 알파벳이 같은경우
+			if(length == 1 && n==10)//모든 알파벳이 같은경우
 				minLen = Math.min(minLen, 3);
 			else
 				minLen = Math.min(minLen, 2*length);
