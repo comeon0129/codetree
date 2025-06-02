a6117dc382a20e884b1af636c72d495a741361d7
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-06-02 11:13:59 +0900

    Title: 벽 짚고 미로 탈출하기, Time: 3068ms, Memory: 11MB, Status: Time Limit Exceed - Codetree

---
+++ 250602/벽 짚고 미로 탈출하기/escape-maze-with-wall-following.java
@@ -0,0 +1,83 @@
+import java.util.Scanner;
+
+public class Main {
+	public static int n;//미로 크기
+	public static char[][] maze;
+	public static int x,y; //시작 위치
+	
+	public static boolean inRange(int x, int y) {
+		return 1<=x && x<=n && 1<=y && y<=n;
+	}
+	
+	public static int exit() {
+		int[] dx = {0,-1,0,1}; // 오른쪽, 위, 왼쪽, 아래 순서 (반시계방향)
+		int[] dy = {1,0,-1,0};
+		
+		int exitTime = 0;
+		int dirNum = 0;
+		
+		int org_x = x;//시작 위치 
+		int org_y = y;//시작 위치
+		
+		while(true) {
+			int nx = x+dx[dirNum]; int ny = y+dy[dirNum];
+			
+			if(!inRange(nx,ny)) {
+				exitTime++;
+				break;
+			}
+			
+			//바라보고 있는 방향으로 한칸 갔을때 벽이 있는 경우 반시계방향으로 90도 회전
+			if(maze[nx][ny] == '#') {
+				dirNum = (dirNum+1) % 4;
+				continue;
+			}
+			
+			//이동했을때 해당방향 기준으로 오른쪽에 짚을 벽이 있는 경우
+			if(maze[nx+dx[(dirNum+3)%4]][ny+dy[(dirNum+3)%4]] == '#') {
+				exitTime++;
+			}
+			
+			else {
+				x= nx;
+				y= ny;
+				dirNum = (dirNum+3) % 4;
+				nx = x+ dx[dirNum];
+				ny = y+dy[dirNum];
+				exitTime+=2;
+			}
+			
+			x = nx;
+			y = ny;
+			
+			//초기 위치로 돌아왔고 다시 같은 방향으로 움직임을 반복하는 경우
+			if(x== org_x && y== org_y && dirNum == 0)
+				return -1;
+			
+		}
+		
+		return exitTime;
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		maze = new char[n+1][n+1];
+		
+		x = sc.nextInt();
+		y = sc.nextInt();
+		
+		for(int i=1; i<=n; i++){
+			String line = sc.next();
+			for(int j=1; j<=n; j++) {
+				maze[i][j] = line.charAt(j-1);
+			}
+		}
+		
+		int exitTime = exit();
+		
+		System.out.println(exitTime);
+		
+		sc.close();
+	}
+}
