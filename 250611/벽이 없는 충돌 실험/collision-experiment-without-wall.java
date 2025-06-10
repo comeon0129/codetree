e9697a1c1516bf09998b1fee7794ce86695da1ed
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-06-11 01:26:40 +0900

    Title: 벽이 없는 충돌 실험, Time: 5123ms, Memory: 193MB, Status: Time Limit Exceed - Codetree

--- 250611/벽이 없는 충돌 실험/collision-experiment-without-wall.java
+++ 250611/벽이 없는 충돌 실험/collision-experiment-without-wall.java
@@ -1,6 +1,11 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 class Marble {
 	double x, y;
@@ -71,28 +76,32 @@ public class Main {
 		
 	}
 	
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		t = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		t = Integer.parseInt(br.readLine());
+		
 		while(t-->0) {
-			n = sc.nextInt();
+			n = Integer.parseInt(br.readLine());
 			time = 0;
 			ans = -1;
 			marbles = new ArrayList<>();
 			
 			for(int i=1; i<=n; i++) {
-				double x = sc.nextDouble();
-				double y = sc.nextDouble();
-				int w = sc.nextInt();
-				char d = sc.next().charAt(0);
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				double x = Double.parseDouble(st.nextToken());
+				double y = Double.parseDouble(st.nextToken());
+				int w = Integer.parseInt(st.nextToken());
+				String d = st.nextToken();
 				
 				int dirNum = -1;
 				
-				if(d == 'U')
+				if(d.equals("U"))
 					dirNum = 0;
-				else if(d == 'D')
+				else if(d.equals("D"))
 					dirNum = 1;
-				else if(d == 'R')
+				else if(d.equals("R"))
 					dirNum = 2;
 				else
 					dirNum =3;
@@ -106,10 +115,11 @@ public class Main {
 				simulate();
 			}
 			
-			System.out.println(ans);
+			bw.write(ans+" ");
+			bw.write('\n');
+			
 		}
-		
-		sc.close();
+		bw.flush();
+		bw.close();
 	}
 }
-
