c32aa48cde388a83e84203503259f6cc9d989819
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-06-19 02:09:13 +0900

    Title: 최대 합 분할, Time: 247ms, Memory: 26MB, Status: Passed - Codetree

--- 250619/최대 합 분할/maximum-sum-partition.java
+++ 250619/최대 합 분할/maximum-sum-partition.java
@@ -5,11 +5,19 @@ public class Main {
 	public static int[] arr;
 	public static int[][] dp; //dp[i][j] : i번째 수까지 고려했을때 A그룹 합 - B그룹 합이 j일때의 최대 A그룹 합
 	
+	public static int sum = 0;
+	
+	public static void update(int i, int j, int prevI, int prevJ, int val) {
+		if(prevJ < 0 || prevJ >2*sum || dp[prevI][prevJ] == Integer.MIN_VALUE)
+			return;
+		
+		dp[i][j] = Math.max(dp[i][j], dp[prevI][prevJ]+val);
+	}
+	
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		n = sc.nextInt();
 		arr = new int[n+1];
-		int sum = 0;
 		
 		
 		for(int i=1; i<=n; i++) {
@@ -21,32 +29,26 @@ public class Main {
 
 		for (int i = 0; i <= n; i++) {
 		    for (int j = 0; j <= 2*sum; j++) {
-		        dp[i][j] = -1; // 불가능한 상태로 초기화
+		        dp[i][j] = Integer.MIN_VALUE;
 		    }
 		}
+		
 		dp[0][OFFSET] = 0; // 차이가 0인 시작 상태
 
-		for (int i = 1; i <= n; i++) {
-		    for (int j = 0; j <= 2*sum; j++) {
-		        if (dp[i-1][j] == -1) continue; // 이전 상태가 불가능하면 skip
-
-		        // case 1: i번째 수를 아무 그룹에도 안 넣음 (C그룹)
-		        dp[i][j] = Math.max(dp[i][j], dp[i-1][j]);
-
-		        // case 2: B 그룹에 넣는 경우 => 차이 감소
-		        if (j - arr[i] >= 0)
-		            dp[i][j - arr[i]] = Math.max(dp[i][j - arr[i]], dp[i-1][j]);
-
-		        // case 3: A 그룹에 넣는 경우 => 차이 증가 + A합 증가
-		        if (j + arr[i] <= 2*sum)
-		            dp[i][j + arr[i]] = Math.max(dp[i][j + arr[i]], dp[i-1][j] + arr[i]);
-		    }
+		for(int i=1; i<=n; i++) {
+			for(int j=0; j<=2*sum; j++) {
+//				case1. i번째 수를 A그룹에 넣는 경우
+				update(i,j, i-1, j-arr[i], arr[i]);
+				
+//				case2. i번째 수를 B그룹에 넣는 경우
+				update(i,j,i-1, j+arr[i], 0);
+				
+//				case3. i번째 수를 C그룹에 넣는 경우
+				update(i,j,i-1,j,0);	
+			}
 		}
-
-		System.out.println(dp[n][OFFSET]); // 차이가 0인 경우의 최대 A 그룹 합
 		
-		
-		
+		System.out.println(dp[n][OFFSET]);
 		sc.close();
 	}
 }
