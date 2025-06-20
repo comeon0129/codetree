a800f87141ab7d6672056d73c1bd20895089aba3
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-06-20 13:28:21 +0900

    Title: 계단 오르기 2, Time: 153ms, Memory: 12MB, Status: Wrong Answer - Codetree

---
+++ 250620/계단 오르기 2/climbing-stairs-2.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+	public static int n;
+	public static int[] coins;
+	public static int[][] dp; //dp[i][j]: i번째 층을 밟았을때 현재까지 1계단 단위로 오른 횟수가 j일때 얻을 수 있는 최대 동전 개수
+	
+	public static void initalize() {
+		for(int i=0; i<=n; i++) {
+			for(int j=0; j<4; j++)
+				dp[i][j] = Integer.MIN_VALUE;
+		}
+		dp[0][0] = 0;
+		
+		for(int i=1; i<=n; i++) {
+			
+		}
+	}
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+		coins = new int[n+1];
+		dp = new int[n+1][4];
+		for(int i=1; i<=n; i++)
+			coins[i] = sc.nextInt();
+		
+		for(int i=1; i<=n; i++) {
+			for(int j=0; j<4; j++) {
+				
+				if(j>=1 && dp[i-1][j-1] != Integer.MIN_VALUE)
+					dp[i][j] = Math.max(dp[i][j], dp[i-1][j-1]+coins[i]);
+				
+				if(i>=2 && dp[i-2][j] != Integer.MIN_VALUE)
+					dp[i][j] = Math.max(dp[i][j], dp[i-2][j]+coins[i]);
+			}
+		}
+		
+		System.out.println(dp[n][3]);
+		
+		
+		sc.close();
+	}
+	
+}
