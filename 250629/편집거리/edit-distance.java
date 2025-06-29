dd97f9bbded130360906258f272b4bc212c00e76
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-06-29 17:22:01 +0900

    Title: 편집거리, Time: 213ms, Memory: 15MB, Status: Passed - Codetree

---
+++ 250629/편집거리/edit-distance.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+public class Main {
+	public static int[][] dp ;
+	public static String a;
+	public static String b;
+	
+	
+	public static void initalize() {
+		int n = a.length();
+		int m = b.length();
+		
+		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<m; j++) {
+				dp[i][j] = Integer.MIN_VALUE;
+			}
+		}
+		
+		
+		if(a.charAt(0) == b.charAt(0)) {
+			dp[0][0] = 0;
+		}
+		else
+			dp[0][0] = 1;
+		
+//		가로로 맨 윗줄 채우기 
+		for(int j=1; j<m; j++) {
+			if(a.charAt(0) == b.charAt(j))
+				dp[0][j] = j;
+			else
+				dp[0][j] = dp[0][j-1]+1;
+		}
+		
+//     세로로 맨 왼쪽 줄 채우기
+		for(int i=1; i<n; i++) {
+			if(b.charAt(0) == a.charAt(i))
+				dp[i][0] = i;
+			else
+				dp[i][0] = dp[i-1][0]+1;
+		}
+	}
+	
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        a = sc.next();
+        b = sc.next();
+        
+        int n = a.length();
+        int m = b.length();
+        
+        dp = new int[n][m];
+        initalize();
+        
+        for(int i=1; i<n; i++) {
+        	for(int j=1; j<m; j++) {
+        		if(a.charAt(i) == b.charAt(j))
+        			dp[i][j] = dp[i-1][j-1];
+        		else {
+        			dp[i][j] = Math.min(dp[i-1][j], Math.min(dp[i][j-1], dp[i-1][j-1]))+1;
+        		}
+        	}
+        }
+        
+        System.out.println(dp[n-1][m-1]);
+        
+        sc.close();
+    }
+}
