4ab7b9f4f2320c00dd94715c00669e13f096d3d3
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-07-01 14:06:17 +0900

    Title: 올바른 등식 만들기, Time: 126ms, Memory: 10MB, Status: Wrong Answer - Codetree

--- 250701/올바른 등식 만들기/right-equality.java
+++ 250701/올바른 등식 만들기/right-equality.java
@@ -1,13 +1,34 @@
+
 import java.util.Scanner;
+
 public class Main {
+	public static int n,m;
+	public static int[] num;
+	public static long[][] dp;
+	
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt();
-        int M = sc.nextInt();
-        int[] numbers = new int[N];
-        for (int i = 0; i < N; i++) {
-            numbers[i] = sc.nextInt();
+        n = sc.nextInt();
+        m = sc.nextInt();
+        dp = new long[n+1][41]; // dp[i][j]: i번째 숫자까지 고려했을때, 합이 j가 되게 하는 가짓수
+        num = new int[n+1];
+        for(int i=1; i<=n; i++)
+        	num[i] = sc.nextInt();
+        dp[1][20-num[1]] =1;
+        dp[1][20+num[1]] = 1;
+        
+        for(int i=1; i<n; i++) {
+        	for(int j=0; j<=40; j++) {
+        		if(0<= j-num[i+1] )
+        			dp[i+1][j-num[i+1]] += dp[i][j];
+        		
+        		if(j+num[i+1] <=40)
+        			dp[i+1][j+num[i+1]] += dp[i][j];
+        	}
         }
-        // Please write your code here.
+        
+        System.out.println(dp[n][m+20]);
+        
+        sc.close();
     }
 }
