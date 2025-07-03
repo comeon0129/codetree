4afe6beb499db8a2ac9f8a88eba77c39e1147c04
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-07-03 15:01:48 +0900

    Title: 대칭 차집합, Time: 132ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 250703/대칭 차집합/symmetric-difference-set.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+import java.util.Scanner;
+public class Main {
+	public static int n,m;
+	
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        HashSet<Integer> s = new HashSet<>();
+        while(n-->0) {
+        	int num = sc.nextInt();
+        	s.add(num);
+        }
+        
+        m = sc.nextInt();
+        while(m-->0) {
+        	int num = sc.nextInt();
+        	s.add(num);
+        }
+        
+        System.out.println(2*s.size()-n-m);
+        
+        sc.close();
+    }
+}
