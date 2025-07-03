866eb3e4fd27d1ce745dfcc108220b1be0bb87b6
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-07-03 14:54:23 +0900

    Title: 데이터 비교, Time: 1551ms, Memory: 52MB, Status: Passed - Codetree

---
+++ 250703/데이터 비교/data-comparison.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Scanner;
+public class Main {
+	public static int n,m;
+	
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        HashSet<Integer> s = new HashSet<>();
+        while(n-->0) {
+        	int num = sc.nextInt();
+        	s.add(num);
+        }
+        
+        m = sc.nextInt();
+        while(m-->0) {
+        	int num = sc.nextInt();
+        	
+        	if(s.contains(num)) {
+        		System.out.print(1+" ");
+        	}
+        	else
+        		System.out.print(0+" ");
+        }
+        
+        sc.close();
+    }
+}
