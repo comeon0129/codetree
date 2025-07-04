8076788e2b3479a4f86c72cb5db4f809d57fc9d5
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-07-04 11:03:43 +0900

    Title: 정수 명령어, Time: 1286ms, Memory: 51MB, Status: Passed - Codetree

--- 250704/정수 명령어/Integer-command.java
+++ 250704/정수 명령어/Integer-command.java
@@ -5,9 +5,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        TreeSet<Integer> s= new TreeSet<>();
+       
         
        while(t-->0) {
+    	   TreeSet<Integer> s= new TreeSet<>();
     	   int k = sc.nextInt();
     	   
     	   while(k-->0) {
