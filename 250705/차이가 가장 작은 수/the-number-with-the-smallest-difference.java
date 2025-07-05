44592ce91b91c2a0d0319e22e7745089eace923a
GyonYoung <69848468+comeon0129@users.noreply.github.com>
2025-07-05 15:43:05 +0900

    Title: 차이가 가장 작은 수, Time: 129ms, Memory: 10MB, Status: Wrong Answer - Codetree

---
+++ 250705/차이가 가장 작은 수/the-number-with-the-smallest-difference.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n =sc.nextInt();
+        int m = sc.nextInt();
+        TreeSet<Integer> s = new TreeSet<>();
+        
+        int diff = Integer.MAX_VALUE;
+        
+        while(n-->0) {
+        	int num = sc.nextInt();
+        	s.add(num);
+        	
+        	if(s.lower(num) != null) {
+        		
+        		if(num - s.lower(num) >= m)
+        			diff = Math.min(diff, num-s.lower(num));
+        	}
+        	
+        	if(s.higher(num) != null) {
+        		
+        		if(s.higher(num) - num >= m)
+        			diff = Math.min(diff, s.higher(num) - num);
+        	}
+        
+        }
+       
+        System.out.println(diff);
+        
+        sc.close();
+    }
+}
